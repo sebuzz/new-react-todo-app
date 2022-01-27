d6e3b953252b932e97ee296bc297457090080a04
Gregor Adams <greg@pixelass.com>
2022-01-27 09:39:26 +0100

    feat(server): add error handling

--- server.js
+++ server.js
@@ -15,62 +15,82 @@ app.get("/", (request, response) => {
 
 const DATABASE_URI = "./database/database.json";
 
-app.get("/api/todos", async (request, response) => {
-	const data = await readFile(DATABASE_URI, "utf8");
-	const json = JSON.parse(data);
-	response.json(json.todos);
+app.get("/api/todos", async (request, response, next) => {
+	try {
+		const data = await readFile(DATABASE_URI, "utf8");
+		const json = JSON.parse(data);
+		response.json(json.todos);
+	} catch (error_) {
+		next(error_);
+	}
 });
 
-app.post("/api/todos", async (request, response) => {
-	const data = await readFile(DATABASE_URI, "utf8");
-	const json = JSON.parse(data);
+app.post("/api/todos", async (request, response, next) => {
+	try {
+		const data = await readFile(DATABASE_URI, "utf8");
+		const json = JSON.parse(data);
 
-	const todo = {
-		...request.body,
-		isChecked: false,
-		id: uuid(),
-	};
+		const todo = {
+			...request.body,
+			isChecked: false,
+			id: uuid(),
+		};
 
-	json.todos.push(todo);
-	await writeFile(DATABASE_URI, JSON.stringify(json, null, 4));
-	response.status(201);
-	response.json(todo);
+		json.todos.push(todo);
+		await writeFile(DATABASE_URI, JSON.stringify(json, null, 4));
+		response.status(201);
+		response.json(todo);
+	} catch (error_) {
+		next(error_);
+	}
 });
 
-app.delete("/api/todos", async (request, response) => {
+app.delete("/api/todos", async (request, response, next) => {
 	const { id } = request.body;
-	const data = await readFile(DATABASE_URI, "utf-8");
-	const json = JSON.parse(data);
-	const index = json.todos.findIndex(user => user.id === id);
-	if (index < 0) {
-		throw new Error("This entry does not exist");
+	try {
+		const data = await readFile(DATABASE_URI, "utf-8");
+		const json = JSON.parse(data);
+		const index = json.todos.findIndex(user => user.id === id);
+		if (index < 0) {
+			response.status(400);
+			response.json({ error: { message: "This entry does not exist" } });
+			return;
+		}
+		json.todos.splice(index, 1);
+		await writeFile(DATABASE_URI, JSON.stringify(json, null, 4));
+		// Send a 204 (No Content)
+		response.status(204);
+		response.send();
+		// Or 200
+		// response.status(200);
+		// response.send("entry deleted");
+	} catch (error_) {
+		next(error_);
 	}
-	json.todos.splice(index, 1);
-	await writeFile(DATABASE_URI, JSON.stringify(json, null, 4));
-	// Send a 204 (No Content)
-	response.status(204);
-	response.send();
-	// Or 200
-	// response.status(200);
-	// response.send("entry deleted");
 });
 
-app.put("/api/todos", async (request, response) => {
-	const { id, update } = request.body;
-	const data = await readFile(DATABASE_URI, "utf-8");
-	const json = JSON.parse(data);
-	const index = json.todos.findIndex(user => user.id === id);
-	if (index < 0) {
-		throw new Error("This entry does not exist");
+app.put("/api/todos", async (request, response, next) => {
+	try {
+		const { id, update } = request.body;
+		const data = await readFile(DATABASE_URI, "utf-8");
+		const json = JSON.parse(data);
+		const index = json.todos.findIndex(user => user.id === id);
+		if (index < 0) {
+			response.status(400);
+			response.json({ error: { message: "This entry does not exist" } });
+			return;
+		}
+		json.todos[index] = { ...json.todos[index], ...update, id };
+		await writeFile(DATABASE_URI, JSON.stringify(json, null, 4));
+		// Send a 200
+		response.status(200);
+		response.send(json.todos[index]);
+		// Or 204 (No Content)
+		// response.status(204);
+		// response.send();
+	} catch (error_) {
+		next(error_);
 	}
-	json.todos[index] = { ...json.todos[index], ...update, id };
-	await writeFile(DATABASE_URI, JSON.stringify(json, null, 4));
-	// Send a 200
-	response.status(200);
-	response.send(json.todos[index]);
-	// Or 204 (No Content)
-	// response.status(204);
-	// response.send();
 });
 
 app.listen(port, () => {
